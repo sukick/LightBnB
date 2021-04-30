f9f81fdf4f410abc60c5e123af9b021ab3589594
suki.ck <suki.ckchan@gmail.com>
2021-04-30 09:32:30 +0000

    Add reservation function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -74,8 +74,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  const queryString = `
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`;
+
+const values = [guest_id, limit]
+  return pool.query(queryString, values)
+  .then(res => res.rows)
+
+};
 
 exports.getAllReservations = getAllReservations;
 
